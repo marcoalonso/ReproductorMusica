471dc88128ac3e737bdf9798db76d0934d525d04
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-03-03 09:54:23 -0600

    Celda Cancion agregada

---
+++ ReproductorMusica/CancionCell.swift
@@ -0,0 +1,23 @@
+//
+//  CancionCell.swift
+//  ReproductorMusica
+//
+//  Created by Marco Alonso Rodriguez on 03/03/23.
+//
+
+import UIKit
+
+class CancionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
